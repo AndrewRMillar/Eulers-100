ee4bd65e6ae4e2feb7f04fc791e9f4610ff5cbc9
Andrew Millar <duckman.andrew@gmail.com>
2020-04-26 13:53:02 +0200

    Finished problem 5

--- problem005.js
+++ problem005.js
@@ -1,6 +1,21 @@
+// What is the smallest positive number that is evenly 
+// divisible by all of the numbers from 1 to n?
 function smallestMult(n) {
-  // Good luck!
-  return true;
+  
+  let found = false;
+  let num = 0;
+  while(found == false) {
+    found = true;
+    num++;
+    for (let i = 1; i <= n; i++) {
+      if (num % i !== 0) {
+        found = false;
+        break;
+      }
+      
+    }
+  }
+  return num;
 }
 
-smallestMult(20); // 232792560
+smallestMult(20); // 232792560 232792561
