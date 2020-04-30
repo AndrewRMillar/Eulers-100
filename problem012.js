129a1af1ebfb95d96fb82b596442e414c2688e88
Andrew Millar <duckman.andrew@gmail.com>
2020-04-30 13:41:51 +0200

    Working on problem 13

--- problem012.js
+++ problem012.js
@@ -1,6 +1,10 @@
 // Project Euler: Problem 12: Highly divisible triangular number
 // https://projecteuler.net/problem=12
 
+// Discord: bright-augury;
+// Sorry, suppose you want to count the divisors of 10. you need only iterate through the square root of 10, which is 3.16 . 1 is evenly divisble by 10, so that is one pair (1 and 10), 2 is divisible by 10 so that is another (2 and 5). 3 is not. Because 4 is greater than 3.16, we know that if it were divisible by 10 we would have already counted it
+// So in other words, for each i we add 2 to our divisorCount
+
 /**
  * Helper function to get the number of factors for a given number
  * @param {int} num Number for which the number of factors should be found
@@ -12,7 +16,7 @@ const factorsNum = (num) => {
   for(let i = 1; i <= num/sqrtNum; i++) {
     if (num % i === 0) amount++;
   }
-  return amount+1;
+  return amount*2;
 };
 
 /**
